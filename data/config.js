057be46f23b106cfc1a61f8fab69a306cc2f6058
kxm0njae <kxm0njae@gmail.com>
2024-06-19 02:15:53 +0900

    last_commit

--- data/config.js
+++ data/config.js
@@ -12,40 +12,64 @@ export const projects = {
   desc: 'This is a web projects I`ve worked on.',
   projects: [
     {
-      title: '[Web] RΛZΞR Store',
+      title: '[JBU_Demo] JBU_Web_Demo',
+      description: '2024_JBU_First_project',
+      link: 'gallery-beta-navy.vercel.app',
+      github: 'kxm0njae/JBU_WebDemo'
+    },
+    {
+      title: '[MID_Term_Exam] MID_Personal_Project',
+      description: '2024 MID_Personal_Project',
+      link: 'jbu-personal-homepage.vercel.app',
+      github: 'kxm0njae/JBU_team_homepage'
+    },
+    {
+      title: '[MID_Term_Exam] MID_Team_Project',
+      description: '2024 MID_Team_Project',
+      link: 'jbu-team-homepage.vercel.app',
+      github: 'kxm0njae/gallery'
+    },
+    {
+      title: '[Final_Exam] Final_Team_Project',
+      description: '2024 Final_Team_Project',
+      link: 'jbufinalteam.netlify.app',
+      github: 'kxm0njae/team2-main'
+    },
+    {
+      title: '[RAZER_Theme] RΛZΞR Store',
       description: 'Click on an image to expand it:',
       link: 'gallery-beta-navy.vercel.app',
       github: 'kxm0njae/gallery'
     },
     {
-      title: '[Web] P/N Registration',
+      title: '[RAZER_Theme] P/N Registration',
       description: 'RΛZΞR P/N Registration',
       link: 'book-ruddy-sigma.vercel.app',
-      github: '/kxm0njae/book'
+      github: 'kxm0njae/book'
     },
     {
-      title: '[Web] User Management',
-      description: 'Add User',
-      link: 'vercel',
-      github: 'github'
+      title: '[RAZER_Theme] KC Management',
+      description: 'RΛZΞR KC authentication',
+      link: 'user-eosin.vercel.app',
+      github: 'kxm0njae/user'
     },
     {
       title: '[Web] Restaurant Menu Management',
       description: 'Chinese restaurant menu',
-      link: 'vercel',
-      github: 'github'
+      link: 'jbumenu.netlify.app',
+      github: 'kxm0njae/menu'
     },
     {
       title: '[Web] Table Tennis Score Management',
       description: 'Blue Team vs Red Team',
-      link: 'vercel',
-      github: 'github'
+      link: 'jbuscore.netlify.app',
+      github: 'kxm0njae/score'
     },
     {
       title: '[Web] Examples of QR code usage',
       description: 'QRcode.js',
-      link: 'vercel',
-      github: 'github'
+      link: 'jbuqrcode.netlify.app',
+      github: 'kxm0njae/qrcode'
     }
   ]
 };
